08b62df537f1566474fb389ca461c9eab225584f
Luizhdevs <luizhdevs00@gmail.com>
2023-09-20 10:05:34 -0300

    feat: create api food-delivery

---
+++ src/lib/api.ts
@@ -0,0 +1,23 @@
+// import axios from "axios";
+// import dotenv from "dotenv"
+
+// dotenv.config()
+
+// export const api = axios.create({
+//   baseURL: process.env.ASAAS_API_URL,
+//   headers: {
+//     access_token: process.env.ASSAS_API_ACCESS_TOKEN,
+//   },
+// })
+
+import axios from "axios"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+export const api = axios.create({
+  baseURL: process.env.ASAAS_API_URL,
+  headers: {
+    access_token: process.env.ASAAS_API_ACCESS_TOKEN,
+  },
+})
